00d31ce4e509dd9c0b81520b75d4ca3baf8c8517
MarianneGarabetian <margara@student.chalmers.se>
2024-11-11 15:53:49 +0000

    Coordinator for neos api (task 1.4 partly) *group effort*

--- homeassistant/components/nasa_api/__init__.py
+++ homeassistant/components/nasa_api/__init__.py
@@ -8,6 +8,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import CONF_API_KEY, DEFAULT_API_KEY, DOMAIN, LOGGER
+from .coordinator import NasaDataUpdateCoordinator
 from .nasa_api_client import NasaApiClient
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
@@ -29,8 +30,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         api_key=entry.data.get(CONF_API_KEY, DEFAULT_API_KEY), session=session
     )
 
-    # Store api client in hass data
-    hass.data[DOMAIN][entry.entry_id] = api_client
+    nasa_coordinator = NasaDataUpdateCoordinator(hass, api_client)
+    await nasa_coordinator.async_config_entry_first_refresh()
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = nasa_coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
--- homeassistant/components/nasa_api/coordinator.py
+++ homeassistant/components/nasa_api/coordinator.py
@@ -1,1 +1,39 @@
 """Coordinator for NASA API."""
+
+from datetime import timedelta
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import DOMAIN
+from .models import NeoWsAsteroid
+from .nasa_api_client import NasaApiClient
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class NasaDataUpdateCoordinator(DataUpdateCoordinator[list[NeoWsAsteroid]]):
+    """Class to manage the NEO data fetching from the API."""
+
+    def __init__(self, hass: HomeAssistant, client: NasaApiClient) -> None:
+        """Initialize the NASA data update coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=DOMAIN + "_NEO_COORDINATOR",
+            update_interval=timedelta(seconds=10),  # 10 sec
+        )
+        self.api_client = client
+        self.cache: dict[str, NeoWsAsteroid] = {}
+
+    async def _async_update_neows_data(self) -> list[NeoWsAsteroid]:
+        """Fetch data from Neo API."""
+        try:
+            return await self.api_client.fetch_neos_data()
+        except Exception as err:
+            raise UpdateFailed(f"Error fetching data: {err}") from err
+
+    async def _async_update_data(self) -> list[NeoWsAsteroid]:
+        """Override method in HomeAssistants DataUpdateCoordinator to make data available to sensors."""
+        return await self._async_update_neows_data()
